572cf426add77be3b7618cf7de95322c6e889d28
Rohan Dugad <rohand2468@gmail.com>
2021-01-28 09:25:43 -0500

    Cleaned up code

--- serialwrapper.py
+++ serialwrapper.py
@@ -1,17 +1,22 @@
-import serial
+#!/usr/bin/env python3
+"""Serial Protocol Library"""
+
 import enum
-import numpy as np
+import serial
 
 class SerialWrapper:
+    """Class containing usage methods of the Serial Protocol"""
+
     @classmethod
     def begin(cls, serial_port, baud_rate):
+        """Initialized the serial port and defines important data"""
         cls._serial = serial.Serial(serial_port, baud_rate)
 
-        cls._CRC_CALCULATOR =   [0,
-                                3, 6, 5, 7, 4, 1, 2, 5, 6, 3,
-                                0, 2, 1, 4, 7, 1, 2, 7, 4, 6,
-                                5, 0, 3, 4, 7, 2, 1, 3, 0, 5,
-                                6]
+        cls._CRC_CALCULATOR = [0,
+                               3, 6, 5, 7, 4, 1, 2, 5, 6, 3,
+                               0, 2, 1, 4, 7, 1, 2, 7, 4, 6,
+                               5, 0, 3, 4, 7, 2, 1, 3, 0, 5,
+                               6]
 
         cls._DELIMITER_BYTE = 0x1f
         cls._DELIMITER_BYTE_PCS = cls._process(cls._DELIMITER_BYTE)
@@ -22,9 +27,10 @@ class SerialWrapper:
         cls._itemNum = 0
 
     @classmethod
-    def send(cls, buffer):
+    def send(cls, packet):
+        """Sends packet with protocol"""
         cls._serial.write(cls._DELIMITER_BYTE_PCS)
-        for item in buffer:
+        for item in packet:
             if item > 0x1f:
                 while True:
                     print("[ERROR] TRYING TO SEND BYTE BIGGER THAN 0x1f")
@@ -37,16 +43,17 @@ class SerialWrapper:
         cls._serial.write(cls._DELIMITER_BYTE_PCS)
 
     @classmethod
-    def receive(cls, buffer):
+    def receive(cls, packet):
+        """Receive packet with protocol"""
         while cls._serial.in_waiting:
             message = cls._unprocess(cls._serial.read())
             if message != -1:
-                status, buffer = cls._receiveSM(buffer, message)
+                status, packet = cls._receiveSM(packet, message)
                 if status:
                     if cls._itemNum:
                         itemNum = cls._itemNum
                         cls._itemNum = 0
-                        return buffer, itemNum
+                        return packet, itemNum
             else:
                 cls._state = cls._State.INIT
                 cls._itemNum = 0
@@ -55,6 +62,7 @@ class SerialWrapper:
 
     @classmethod
     def _receiveSM(cls, buffer, byte_in):
+        """Facilitate protocol with state machine"""
         if cls._state == cls._State.INIT:
             if byte_in == cls._DELIMITER_BYTE:
                 cls._state = cls._State.NORMAL
@@ -76,10 +84,12 @@ class SerialWrapper:
 
     @classmethod
     def _doCRC(cls, message):
+        """Add CRC to message"""
         return message * 8 + cls._CRC_CALCULATOR[message]
 
     @classmethod
     def _undoCRC(cls, crc_byte):
+        """Remove CRC and check for corruption"""
         message = (crc_byte & 0xf8) // 8
         crc = crc_byte & 0x07
 
@@ -91,25 +101,31 @@ class SerialWrapper:
 
     @classmethod
     def _escape(cls, raw):
+        """XOR byte with predefined mask"""
         return raw ^ cls._CONVERSION
 
     @classmethod
     def _unescape(cls, escaped):
+        """XOR byte with predefined mask"""
         return escaped ^ cls._CONVERSION
-    
+
     @classmethod
     def _process(cls, byte):
+        """Convert to byte with CRC"""
         return bytes([cls._doCRC(byte)])
 
     @classmethod
     def _unprocess(cls, byte):
+        """Convert to number and undo CRC"""
         return cls._undoCRC(ord(byte))
-    
+
     @classmethod
-    def getSerial(cls):  #Remove
+    def getSerial(cls):
+        """Procide serial object"""
         return cls._serial
-    
+
     class _State(enum.Enum):
+        """Enum for states of Serial protocol"""
         INIT = 0
         NORMAL = 1
         ESCAPE = 2
