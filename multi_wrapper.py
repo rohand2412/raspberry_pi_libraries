5e2f84db2ffb9c859c7a827ba1d3d42b803c08b4
Rohan Dugad <rohand2468@gmail.com>
2021-04-02 21:31:07 -0400

    Implemented load label map method from tensorflow example

--- multi_wrapper.py
+++ multi_wrapper.py
@@ -10,6 +10,7 @@ import argparse
 import numpy as np
 import cv2
 import pynput
+import re
 
 class Packages:
     """Contains basic framework of all modules utilized in this directory"""
@@ -506,3 +507,20 @@ class Packages:
                 files_cur_index += 1
 
             return files_ordered
+
+        @staticmethod
+        def load_label_map(path):
+            """
+            Loads the labels file. Supports files with or without index numbers.
+            From Tensorflow Example Code.
+            """
+            with open(path, 'r', encoding='utf-8') as f:
+                lines = f.readlines()
+                labels = {}
+                for row_number, content in enumerate(lines):
+                    pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
+                    if len(pair) == 2 and pair[0].strip().isdigit():
+                        labels[int(pair[0])] = pair[1].strip()
+                    else:
+                        labels[row_number] = pair[0].strip()
+            return labels
